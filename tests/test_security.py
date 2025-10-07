291c46a4e0eb909ab2d98a11569395dbe41156db
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-07 14:42:39 +0200

    test(security): add unit tests for token decoding

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,7 +1,9 @@
-from todo_api.security import hash_password, verify_password, create_access_token
+from todo_api.security import hash_password, verify_password, create_access_token, decode_access_token, credential_exception
 from todo_api.config import settings
 from datetime import datetime, timezone, timedelta
 from jose import jwt, JWTError
+from fastapi import HTTPException
+import pytest
 
 def test_hash_password():
     password = 'strongpassword'
@@ -56,3 +58,68 @@ def test_create_access_token():
     
     assert token_exp_time < (datetime.now(timezone.utc) + 
                              timedelta(minutes=expire_minutes + 1))
+    
+def test_decode_access_token():
+    
+    # payload creation
+    payload_to_encode = {
+        'sub':'Alice',
+        'id':1,
+        'role':'user',
+        'exp':datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+    }
+    
+    # Token encoding
+    test_token = jwt.encode(
+        payload_to_encode, settings.secret_key, settings.algorithm
+    )
+    
+    # decode_access_token
+    decoded_token = decode_access_token(test_token)
+    
+    assert decoded_token['username'] == payload_to_encode['sub']
+    assert decoded_token['id'] == payload_to_encode['id']
+    assert decoded_token['role'] == payload_to_encode['role']
+    
+def test_decode_access_token_expired():
+    
+    # payload creation
+    payload_to_encode = {
+        'sub':'Alice',
+        'id':1,
+        'role':'user',
+        'exp':datetime.now(timezone.utc) - timedelta(minutes=5)
+    }
+    
+    expired_token = jwt.encode(
+        payload_to_encode, settings.secret_key, settings.algorithm
+    )
+    
+    # verifies that the function raises the correct exception
+    with pytest.raises(HTTPException) as exc_info:
+        decode_access_token(expired_token)
+        
+    # verifies that is credential_exception (401)
+    assert exc_info.value.status_code == credential_exception.status_code
+    assert exc_info.value.detail == credential_exception.detail
+
+def test_decode_access_token_missing_value():
+    
+    # payload creation
+    payload_to_encode = {
+        'sub':'Alice',
+        'id':1,
+        'exp':datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
+    }
+    
+    expired_token = jwt.encode(
+        payload_to_encode, settings.secret_key, settings.algorithm
+    )
+    
+    # verifies that the function raises the correct exception
+    with pytest.raises(HTTPException) as exc_info:
+        decode_access_token(expired_token)
+        
+    # verifies that is credential_exception (401)
+    assert exc_info.value.status_code == credential_exception.status_code
+    assert exc_info.value.detail == credential_exception.detail
