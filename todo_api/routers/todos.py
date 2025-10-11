d642ef0dac154342821961c7412f55622d08fb1f
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-11 15:21:01 +0200

    feat(todos): add endpoint to retrieve user's todos with optional filter

--- todo_api/routers/todos.py
+++ todo_api/routers/todos.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, Query
 
 from todo_api.dependencies import db_dependency, user_dependency
 from todo_api.schema import Message, TodoRequest, TodoUpdateRequest, TodoOutput
@@ -16,3 +16,18 @@ async def create_todo(todo_request: TodoRequest, db: db_dependency,
     db.commit()
     
     return Message(message="Todo created successfully.")
+
+# ================================ Get Todos ================================= #
+@router.get("", status_code=status.HTTP_200_OK, response_model=list[TodoOutput])
+async def read_all_todos(db: db_dependency, user: user_dependency,
+                         complete: bool | None = Query(default=None)) -> list[TodoOutput]:
+    
+    # Base query filtering todos by the authenticated user
+    query = db.query(Todo).filter(Todo.owner_id == user.id)
+    
+    # If 'complete' is provided, further filter the todos
+    if complete is not None:
+        query = query.filter(Todo.complete == complete)
+    
+    # query execution and return results: a list of TodoOutput
+    return query.all()
