9d7fbefb1475649c7e1b45acbafa78f2a271c369
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-11 15:22:44 +0200

    feat(api): add todos router

--- todo_api/api.py
+++ todo_api/api.py
@@ -5,6 +5,7 @@ from todo_api.database import Base, engine
 from todo_api import models
 from todo_api.routers.auth import router as auth_router
 from todo_api.routers.user import router as user_router
+from todo_api.routers.todos import router as todos_router
 from todo_api.config import settings
 
 # FastAPI application initialization
@@ -39,3 +40,4 @@ async def root():
 
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(todos_router)
