73dbeea61083ef75b1bad9cf4b83e443f2d7e7aa
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-15 15:01:35 +0200

    feat(api): add admin router

--- todo_api/api.py
+++ todo_api/api.py
@@ -6,6 +6,7 @@ from todo_api import models
 from todo_api.routers.auth import router as auth_router
 from todo_api.routers.user import router as user_router
 from todo_api.routers.todos import router as todos_router
+from todo_api.routers.admin import router as admin_router
 from todo_api.config import settings
 
 # FastAPI application initialization
@@ -41,3 +42,4 @@ async def root():
 app.include_router(auth_router)
 app.include_router(user_router)
 app.include_router(todos_router)
+app.include_router(admin_router)
