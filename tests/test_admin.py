a2172a5d9507fa66dc351abfa174f59d26de5e36
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-16 15:36:58 +0200

    test(admin): validate all filters on GET /admin/users endpoint
    Implement tests to verify the functionality and correctness of all optional query filters for retrieving the user list as an administrator:
    - role filter: checks for accurate role-based selection.
    - username filter: verifies the partial prefix matching and case-insensitivity.
    - is_active filter: ensures correct segregation of active and inactive accounts using the new test_inactive_user fixture.

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -19,3 +19,39 @@ class TestReadUser:
         response = auth_client.get("/admin/users")
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.json()["detail"] == "Operation requires administrator privileges."
+        
+    def test_read_all_users_role_filter(db, admin_client, test_admin, test_user):
+        
+        # Send GET request
+        response = admin_client.get("/admin/users?role=user")
+        assert response.status_code == status.HTTP_200_OK
+        
+        user_list = response.json()
+        assert len(user_list) == 1
+        
+        assert user_list[0]["role"] == "user"
+        
+    def test_read_all_users_username_filter(db, admin_client, test_admin, test_user):
+        
+        pattern = "TestU"
+        
+        # Send GET request
+        response = admin_client.get(f"/admin/users?username={pattern}")
+        assert response.status_code == status.HTTP_200_OK
+        
+        user_list = response.json()
+        assert len(user_list) == 1
+        
+        assert user_list[0]["username"].startswith(pattern)
+        
+    def test_read_all_users_is_active_filter(db, admin_client, test_admin,
+                                             test_inactive_user):
+        
+        # Send GET request
+        response = admin_client.get("/admin/users?is_active=False")
+        assert response.status_code == status.HTTP_200_OK
+        
+        user_list = response.json()
+        assert len(user_list) == 1
+        
+        assert user_list[0]["is_active"] is False
