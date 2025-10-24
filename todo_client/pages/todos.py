9b1609dc3e58cd983f74fcf10e509e8e20f581a2
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-24 12:08:54 +0200

    feat(frontend.todos): implement initial todos page layout and listing
    Add the base UI for the Todos page:
    - Fetch and display all todos using APIClient.read_all_todos().
    - Show informative messages for empty lists and error states (including 401 handling).
    - Display todos in expandable sections with key details (title, priority, completion).
    - Include placeholder controls for filtering, editing, and deleting todos.

--- todo_client/pages/todos.py
+++ todo_client/pages/todos.py
@@ -5,10 +5,43 @@ def todos_page_content() -> None:
     
     client = st.session_state["api_client"]
     st.title("📝 Todos")
-    st.write("Placeholder text")
     
     result = client.read_all_todos()
     if isinstance(result, dict):
-        st.error(f"Error: {result.get("error")} [status code: {result.get("status_code")}]")
+        status_code = result.get("status_code")
+        st.error(f"Error: {result.get("error")} [status code: {status_code}]")
+        if status_code == 401:
+            st.warning("Expired session. Please log in again.")
     
+    if not result:
+        st.info("Congratulations! You don't have any more tasks to complete.")
+        return
     
+    st.header(f"Your Todos ({len(result)})")
+    st.markdown("---")
+    col1, col2, col3 = st.columns(3, vertical_alignment="bottom")
+    
+    with col1:
+        complete_opt = st.selectbox("Complete", ["--", False, True])
+        
+    with col2:
+        search_opt = st.text_input("Start with:", help="Search todos by title or description")
+    
+    with col3:
+        if st.button("Filter", width=100):
+            st.info("Filter feature comming soon")
+    
+    for i, todo in enumerate(result):
+        
+        extender_text = f"**{todo.get('title')}** - Priority: **{todo.get('priority')}**"
+        extender_text += f" (ID: {todo.get('id')})"
+        extender_icon = "✅" if todo.get('complete') is True else "⏳"
+        
+        with st.expander(extender_text, icon=extender_icon):
+            st.write(f"**Description:** {todo.get('description')}")
+            
+            ext_col1, ext_col2 = st.columns(2)
+            with ext_col1:
+                st.button("Edit", key=f"edit_{i}", width=200)
+            with ext_col2:
+                st.button("Delete", key=f"delete_{i}", width=200)
