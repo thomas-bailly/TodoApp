0980e30c92e90aa8618278cbe5fe5d3a150ee037
thomas-bailly <thomas.bailly1698@gmail.com>
2025-10-28 15:09:21 +0100

    feat(frontend.admin): implement initial admin page layout
    Set up the foundational UI for the Admin Panel page:
    - Introduce three tabs: 'All Users', 'User', and 'Todo'.
    - Implement the 'All Users' tab layout with filtering controls (Username, Role, Active Status) using st.columns.
    - This commit focuses purely on the visual structure and initial state.

--- todo_client/pages/admin.py
+++ todo_client/pages/admin.py
@@ -7,3 +7,34 @@ def admin_page_content():
     
     st.title("🛡️ Admin")
     st.write("Work in progress...🚧")
+    
+    tab1, tab2, tab3 = st.tabs(["All Users", "User", "Todo"])
+    
+    with tab1:
+        col1, col2, col3, col4, col5 = st.columns(5, vertical_alignment="bottom")
+        with col1:
+            username_filter = st.text_input("Username")
+        
+        with col2:
+            role_filter = st.selectbox(
+                "Role",
+                options=["", "user", "admin"]
+            )
+        
+        with col3:
+            is_active_filter = st.selectbox(
+                "Active",
+                options=["", True, False]
+            )
+        
+        with col4:
+            filter_button = st.button("Filter", use_container_width=True)
+            
+        with col5:
+            reset_button = st.button("Reset", use_container_width=True)
+            
+        if "users_list" not in st.session_state:
+            with st.spinner("Loading Users..."):
+                st.session_state["users_list"] = []
+        
+        
